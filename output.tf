38cb055f393386d2e9c63f91c1feaf79f209e884
Duc Nguyen <anhducmata@gmail.com>
2023-02-26 22:19:42 +0700

    no message

--- output.tf
+++
@@ -1,6 +0,0 @@
-output "ec2" {
-  value       = aws_instance.ec2.id
-}
-output "loadbalancer" {
-  value       = aws_lb.loadbalancer.arn
-}
--- terraform.tfvars
+++
@@ -1,3 +0,0 @@
-region = "us-east-1"
-access_key = "XXX"
-secret_key = "XXX"
